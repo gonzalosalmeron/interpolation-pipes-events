eb5ef7d2281f667897fb076ff945f6d24591597b
gonzalo <gonzxlosalmeron@gmail.com>
2021-10-13 08:48:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interpolation-pipes-events',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
